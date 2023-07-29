4c5d05a5513fae40e66b3254a088d4d0c27ced8b
kamiltrzmiel <81704799+kamiltrzmiel@users.noreply.github.com>
2023-07-29 18:20:57 +0000

    Deploying to gh-pages from @ kamiltrzmiel/goit-react-hw-08-phonebook@86abf973d7ab28c16cd4982234a102c0928e11fa 🚀

--- src/components/Container/Container.styled.jsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledContainer = styled.div`
-  max-width: 200px;
-  margin: 0 auto;
-  padding: 0 10px;
-`;
--- src/components/SharedLayout/SharedLayout.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 0 16px;
-
-`;
